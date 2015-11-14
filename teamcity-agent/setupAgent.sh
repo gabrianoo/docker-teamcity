b9ae94cf9d01c3c5c04b0cb9be30d1812f65f4e2
Ahmed Hassanien <ahmed_hassanien@otasys.com>
2015-11-14 19:10:27 +0100

    Ficed the teamcity agent Dockerfile

--- teamcity-agent/setupAgent.sh
+++ teamcity-agent/setupAgent.sh
@@ -5,6 +5,8 @@ wget $TEAMCITY_SERVER/update/buildAgent.zip
 unzip -q -d /opt/agent buildAgent.zip
 rm buildAgent.zip
 chmod +x /opt/agent/bin/agent.sh
+mv /opt/agent/conf/buildAgent.dist.properties /opt/agent/conf/buildAgent.properties
+sed -i "s,http://localhost:8111/,$TEAMCITY_SERVER,g" /opt/agent/conf/buildAgent.properties
 
 # Default command.
 sh /opt/agent/bin/agent.sh run
