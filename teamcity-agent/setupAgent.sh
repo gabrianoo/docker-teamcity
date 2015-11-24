7d0cd2ca03f12d3db24efb345dbb06cc4f12d5b1
Ahmed Hassanien <eng.ahmedgaber@gmail.com>
2015-11-24 22:28:32 +0100

    Update setupAgent.sh

--- teamcity-agent/setupAgent.sh
+++ teamcity-agent/setupAgent.sh
@@ -7,6 +7,12 @@ rm buildAgent.zip
 chmod +x /opt/agent/bin/agent.sh
 mv /opt/agent/conf/buildAgent.dist.properties /opt/agent/conf/buildAgent.properties
 sed -i "s,http://localhost:8111/,$TEAMCITY_SERVER,g" /opt/agent/conf/buildAgent.properties
+# For Unidirection conection (Agent-To-Server)
+sed -i "s,ownPort=9090,#ownPort=9090,g" /opt/agent/conf/buildAgent.properties
+
+if [ ! -z "$AGENT_NAME" ]; then
+    sed -i "s,name=,name=$AGENT_NAME,g" /opt/agent/conf/buildAgent.properties
+fi 
 
 # Default command.
 sh /opt/agent/bin/agent.sh run
