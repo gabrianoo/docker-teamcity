18aac7d4be0243e0aebbb2bf869cafa1be28d561
Ahmed Hassanien <eng.ahmedgaber@gmail.com>
2015-11-25 22:53:30 +0100

    Added fully support https certificates

--- teamcity-agent/setupAgent.sh
+++ teamcity-agent/setupAgent.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir -p /opt/agent
 wget --no-check-certificate $TEAMCITY_SERVER/update/buildAgent.zip
@@ -13,6 +13,11 @@ sed -i "s,ownPort=9090,#ownPort=9090,g" /opt/agent/conf/buildAgent.properties
 if [ ! -z "$AGENT_NAME" ]; then
     sed -i "s,name=,name=$AGENT_NAME,g" /opt/agent/conf/buildAgent.properties
 fi 
+
+if [[ $TEAMCITY_SERVER == https* ]]; then
+    openssl s_client -showcerts -connect mc.facetug.com:443 </dev/null 2>/dev/null|openssl x509 -outform PEM >mycertfile.pem
+    keytool -importcert -noprompt -trustcacerts -file mycertfile.pem -keystore $JAVA_HOME/jre//lib/security/cacerts -storepass changeit
+fi
 
 # Default command.
 sh /opt/agent/bin/agent.sh run
